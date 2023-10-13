97eb50ff3068d92d371b0e070a49fa06734b7f5a
PRADEEPA SENTHILRAJAN <pradeepa.senthil@gmail.com>
2023-10-12 20:18:43 -0400

    Status1

--- src/main/java/org/launchcode/techjobsmvc/controllers/SearchController.java
+++ src/main/java/org/launchcode/techjobsmvc/controllers/SearchController.java
@@ -29,7 +29,7 @@ public class SearchController {
     @PostMapping("results")
     public String displaySearchResults(Model model, @RequestParam String searchType, @RequestParam String searchTerm) {
         ArrayList<Job> jobs;
-        if ((searchType.equals("all")) || (searchTerm == null)) {
+        if ((searchType.equals("all")) && (searchTerm == null)) {
             jobs = JobData.findAll();
 //            model.addAttribute("title","All Jobs");
         } else {
